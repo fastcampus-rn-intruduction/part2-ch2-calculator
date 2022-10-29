19a3de79db1bcdc99c25d91489f4541fed81faeb
daheeahn <deg9810@gmail.com>
2022-10-29 16:25:10 +0900

    [ch2 - 2 - state 세팅: 첫번째항, 연산자, 두번째항, 결과] 최종 코드

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [input, setInput] = useState(0); // 2 -> 14
+  const [currentOperator, setCurrentOperator] = useState(null); // + -> null
+  const [result, setResult] = useState(null); // 12 -> 14
+  const [tempInput, setTempInput] = useState(null); // 2
+  const [tempOperator, setTempOperator] = useState(null); // +
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
